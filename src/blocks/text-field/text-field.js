80196c60f02e83184ea622bb8fb99f89a2a4efb0
Malluma <sudzumia@gmail.com>
2022-01-25 21:07:51 +0700

    changing mixs to wraps; mask text field

---
+++ src/blocks/text-field/text-field.js
@@ -0,0 +1,6 @@
+import $ from "jquery";
+import "jquery-mask-plugin";
+
+$(function(){
+  $('.js-text-field__input_masked').mask('00.00.0000');
+})
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles/index.scss';
 import babelTest from './babel-test_';
+import textField from './blocks/text-field/text-field';
 
 console.log('Hello, I am index.js!!')
 babelTest();
