57fcf30cfb2e29e7f797662d947350746c682435
Malluma <sudzumia@gmail.com>
2022-04-04 16:33:33 +0700

    expandable checkbox

--- src/blocks/checkbox-list/CheckboxList.js
+++ src/blocks/checkbox-list/CheckboxList.js
@@ -3,10 +3,11 @@ class CheckboxList{
   constructor(list, btn) {
     this.list = list;
     this.expandListBtn = btn;
+    this.handleBtnClick = this.handleBtnClick.bind(this);
     this.expandListBtn.addEventListener("click", this.handleBtnClick); 
   }
   
-  handleBtnClick = () => {
+  handleBtnClick() {
     this.expandListBtn.classList.toggle("checkbox-list__btn-expand-rotate");
     this.list.classList.toggle("checkbox-list__hidden");
   };
