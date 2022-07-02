eb190bdfae19a1bd352090bfb55ff6aca238555a
Malluma <sudzumia@gmail.com>
2022-07-02 23:09:22 +0700

    room details page added

---
+++ src/pages/room-details/room-details.js
@@ -0,0 +1,8 @@
+import './room-details.scss';
+
+import '../../page-layout/template/template';
+
+import '../../blocks/booking/booking';
+import '../../blocks/bullet-list/bullet-list';
+import '../../blocks/facility/facility';
+import '../../blocks/review/review';
--- src/pages/search-room/search-room.js
+++ src/pages/search-room/search-room.js
@@ -5,8 +5,6 @@ import '../../page-layout/template/template';
 import '../../blocks/checkbox-list/checkbox-list';
 import '../../blocks/date-dropdown/date-dropdown';
 import '../../blocks/dropdown/dropdown';
-import '../../blocks/footer/footer';
-import '../../blocks/header/header';
 import initPagination from '../../blocks/pagination/pagination';'../../blocks/pagination/pagination';
 import '../../blocks/range-slider/range-slider';
 import '../../blocks/room/room';
