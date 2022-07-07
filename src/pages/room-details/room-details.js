428ce2bd6c09f14eeaf7b08a743fa430ce828ec1
Malluma <sudzumia@gmail.com>
2022-07-07 13:36:54 +0700

    diagram impression added on room details page

--- src/pages/room-details/room-details.js
+++ src/pages/room-details/room-details.js
@@ -4,5 +4,6 @@ import '../../page-layout/template/template';
 
 import '../../blocks/booking/booking';
 import '../../blocks/bullet-list/bullet-list';
+import '../../blocks/diagram/diagram';
 import '../../blocks/facility/facility';
 import '../../blocks/review/review';
