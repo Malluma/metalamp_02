0a53bed60bfa955a3b52e93438953b8cea27d4ee
Malluma <sudzumia@gmail.com>
2022-07-28 22:22:15 +0700

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 "use strict";
 (globalThis["webpackChunkmetalamp_02"] = globalThis["webpackChunkmetalamp_02"] || []).push([["index"],{
 
+/***/ "./src/page-layout/ui-kit-template/ui-kit-template.js":
+/*!************************************************************!*\
+  !*** ./src/page-layout/ui-kit-template/ui-kit-template.js ***!
+  \************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _ui_kit_template_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./ui-kit-template.scss */ "./src/page-layout/ui-kit-template/ui-kit-template.scss");
+
+
+/***/ }),
+
 /***/ "./src/pages/index/index.js":
 /*!**********************************!*\
   !*** ./src/pages/index/index.js ***!
@@ -9,6 +21,20 @@
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.scss */ "./src/pages/index/index.scss");
+/* harmony import */ var _page_layout_ui_kit_template_ui_kit_template__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../page-layout/ui-kit-template/ui-kit-template */ "./src/page-layout/ui-kit-template/ui-kit-template.js");
+
+
+
+/***/ }),
+
+/***/ "./src/page-layout/ui-kit-template/ui-kit-template.scss":
+/*!**************************************************************!*\
+  !*** ./src/page-layout/ui-kit-template/ui-kit-template.scss ***!
+  \**************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
 
 
 /***/ }),
